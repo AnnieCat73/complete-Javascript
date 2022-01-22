6e8c5025ef876d7d48d1c2be3e2357d18fe030f0
Annie <devannie777@gmail.com>
2022-01-22 15:33:55 +0000

    My solution to  challenge 1 part 2

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -36,7 +36,7 @@ if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI of ${BMIMark} is higher than John's BMI of ${BMIJohn}`);
 } else {
   console.log (`John's BMI of ${BMIJohn} is higher than Mark's BMI of ${BMIMark}`);
-}*/
+}
 
 
 //CODING CHALLENGE #3
@@ -69,6 +69,43 @@ if (dolphinBonusScore1 > koalasBonusScore1 && dolphinBonusScore1 >= minScore) {
   console.log(`No team wins!`);
 }
 
+/*CODING CHALLENGE #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can 
+start with an if/else statement, and then try to convert it to a ternary 
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value 
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 
+
+
+const bill = 275;
+const tip1 = bill * 0.15;
+const tip2 = bill * 0.20;
+if (bill <= 300 && bill >= 50) {
+  console.log(`The bill was ${bill} and the tip was ${tip1} and the total value is ${bill + tip1}!`)
+} else {
+  console.log(`The was ${bill} and the tip was ${tip2} and the total value is ${billAmount + tip2}!`);
+}
+
+
+//in ternary
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value 
+${bill + tip}`);
+
+
 
 /* Taking Decisions: if / else Statements
 const age = 15;
@@ -92,4 +129,30 @@ console.log(century);
 */
 
 
+
+//CODING CHALLENGE #1 PART 2
+
+const calcAverageDolphin = (score1, score2, score3) => {
+  return (score1 + score2 + score3)/3;
+}
+//console.log(calcAverageDolphin(44, 23, 71));//46 for Dolphins
+const avgDolphin = calcAverageDolphin(44, 23, 71);
+//console.log(avgDolphin);
+
+const calcAverageKoalas = (score1, score2, score3) => {
+  return (score1 + score2 + score3)/3;
+}
+//console.log(calcAverageKoalas(65, 54, 49));//56 Koalas
+const avgKoalas = calcAverageKoalas(65, 54, 49);
+//console.log(avgKoalas);
+
+const checkWinner = (avgDolphin, avgKoalas) => {
+  if(avgDolphin > avgKoalas) {
+    return `Dolphins wins`;
+  } else if (avgKoalas > avgDolphin) {
+    return `Koalas wins`;
+  }
+}
+
+console.log(checkWinner(avgDolphin, avgKoalas));
 
