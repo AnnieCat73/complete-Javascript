fee32b8afb880a6cb802c811a18c8543d330e949
Annie <devannie777@gmail.com>
2022-03-19 09:41:32 +0000

    Asynchronous definitions

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,25 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+/**
+ * PROMISE DEFINITONS
+ * - An object that is used as a placeholder for the future
+ * result of an async operation 
+ * - A container for an async delivered value
+ * - A container for a future value i.e a response from an
+ AJAX call
+ Promise fullfilled means sucessful but rejected means there
+ was an error
+ * //BETTER: If buy a lottery ticket then it's a PROMISE
+ that I will receive money if I guess the correct outcome so
+ PROMISE: I buy lottery ticket(promise)
+ Lottey draw happens asynchronously
+ If correct outcome, I receive money because it was promised. If I
+ wasn't correct then I will not get any money .i.e rejected(an error
+  occurred in APP f.ex in real ex)
+- In our code we will be able to handle these different states, ie
+fulfilled/rejected*/
+
+const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
+console.log(request)
